229fb3b9a97d34d1da8ee45d98e384886f328bec
Phuwadon <88771136+kappakap39@users.noreply.github.com>
2024-01-10 18:16:01 +0700

    up

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import { RequestHandler } from 'express';
 import prisma from '../lib/db';
 import nodemailer from 'nodemailer';
+import { addDays, startOfDay } from 'date-fns';
 // import * as otpGenerator from 'otp-generator';
 
 // กำหนดค่า nodemailer
@@ -370,32 +371,42 @@ const deleteUser: RequestHandler = async (req, res) => {
 };
 
 const searchUsers: RequestHandler = async (req, res) => {
-    const { query } = req.query as { query: string };
-
     try {
-        const users = await prisma.userManagement.findMany({
+        const { query } = req.query as { query: string };
+        const dateQuery = startOfDay( addDays(new Date(query), +1));
+        const searchConditions: any[] = [
+            { Username: { contains: query.toLowerCase() } },
+            { Userlevel: { contains: query.toLowerCase() } },
+            { Email: { contains: query.toLowerCase() } },
+            { Tel: { contains: query.toLowerCase() } },
+            { Statused: query.toLowerCase() === 'true' },
+            { Question: { contains: query.toLowerCase() } },
+            { Answer: { contains: query.toLowerCase() } },
+            { Title: { contains: query.toLowerCase() } },
+            { Firstname: { contains: query.toLowerCase() } },
+            { Lastname: { contains: query.toLowerCase() } },
+            { Abbreviatename: { contains: query.toLowerCase() } },
+            { CitiZenID: { contains: query.toLowerCase() } },
+            { Effectivedate: { equals: dateQuery } },
+            { Expireddate: { equals: dateQuery } }
+        ];
+
+        console.log('searchConditions', searchConditions);
+
+        const searchResults = await prisma.userManagement.findMany({
             where: {
-                OR: [
-                    { Username: { contains: query.toLowerCase() } },
-                    { Userlevel: { contains: query.toLowerCase() } },
-                    { Question: { contains: query.toLowerCase() } },
-                    { Answer: { contains: query.toLowerCase() } },
-                    { Title: { contains: query.toLowerCase() } },
-                    { Firstname: { contains: query.toLowerCase() } },
-                    { Lastname: { contains: query.toLowerCase() } },
-                    { Abbreviatename: { contains: query.toLowerCase() } },
-                    { Email: { contains: query.toLowerCase() } },
-                    { Tel: { contains: query.toLowerCase() } },
-                    { CitiZenID: { contains: query.toLowerCase() } },
-                    { Picture: { contains: query.toLowerCase() } },
-                    { Statused: { equals: query.toLowerCase() === 'true' } },
-                ],
+                OR: searchConditions,
+            },
+            orderBy: {
+                CreatedAt: 'desc',
             },
         });
 
-        res.json(users);
+        console.log('searchResults', searchResults);
+
+        res.status(200).json({ results: searchResults });
     } catch (error) {
-        console.error(error);
+        console.error('Error during search:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
